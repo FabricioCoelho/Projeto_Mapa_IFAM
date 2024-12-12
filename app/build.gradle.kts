bb117af6cec5cfe28ca31d94ddb864505f4616a9
Fabricio <fabriciocoelho03082000@hotmail.com>
2024-12-11 21:39:36 -0400

    Implementação do banco de dados Firebase na tela de cadastro.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,6 +38,7 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
 }
 
 dependencies {
@@ -47,10 +49,12 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.play.services.maps)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
     // Maps SDK for Android
     implementation(libs.play.services.maps.v1900)
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
 }
--- app/src/main/java/devandroid/fabricio/projeto_mapaifam/MainActivity.kt
+++ app/src/main/java/devandroid/fabricio/projeto_mapaifam/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         //Ação do Botão Entrar
         val botaoEntrar: Button = findViewById(R.id.btEntrar)
         botaoEntrar.setOnClickListener {
